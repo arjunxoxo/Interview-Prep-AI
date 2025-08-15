ee8e232ccce6544cdb8c0450c5643ded295e5142
aayushmehta01 <aayush2511m@gmail.com>
2025-08-15 23:01:27 +0530

    feat: setting up route for register and login

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/db");
-
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// Import routes
+const authRoutes = require("./routes/auth.route");
 
 // Connect to MongoDB
 connectDB();
@@ -15,19 +15,25 @@ connectDB();
 // Middleware
 app.use(cors({
     origin: "*",
-    methods: ["GET,POST,PUT,DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads",express.static(path.join(__dirname, "../client/build")));
+
+// Serve static files from the "uploads" directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 
 app.get("/", (req, res) => {
     res.send("Welcome to the AI Interview Prep Backend!");
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
