8ae4f0a038b1ae1fdecfd0b7170f1e2917171aa7
aayushmehta01 <aayush2511m@gmail.com>
2025-08-15 21:05:37 +0530

    feat: create user schema

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
+
+// Middleware
+app.use(cors({
+    origin: "*",
+    methods: ["GET,POST,PUT,DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads",express.static(path.join(__dirname, "../client/build")));
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the AI Interview Prep Backend!");
