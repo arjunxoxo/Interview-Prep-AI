d2db38fcef55c8234350f59d3b4dc16dfbe78cef
Arjun <arjuntomar941@gmail.com>
2025-08-17 10:04:22 +0530

    question explanation ai api added and tested

--- Backend/controllers/aiController.js
+++ Backend/controllers/aiController.js
@@ -36,7 +36,29 @@ const generateInterviewQuestions = async (req, res) => {
 // @route  POST /api/ai/generate-explanation
 // @access Private
 const generateConceptExplanation = async (req, res) => {
-    
+    try{
+        const { question } = req.body;
+        if (!question) {
+            return res.status(400).json({ success: false, message: 'Question is required' });
+        }
+        const prompt= conceptExplainPrompt(question);
+        const response = await ai.models.generateContent({
+            model: 'gemini-2.5-pro',
+            contents: prompt,
+        });
+        let rawText = response.text;
+        // clean it : Remove ```json and ``` from the start and end
+        const cleanedText = rawText
+        .replace(/^```json\s*/, "") // Remove ```json at the start
+        .replace(/```$/, "") // Remove ``` at the end
+        .trim(); // Trim any extra whitespace
+        // now safe to parse
+        const data = JSON.parse(cleanedText);
+        res.status(200).json({ success: true, data });
+    }catch (error) {
+        console.error('Error generating concept explanation:', error);
+        res.status(500).json({ success: false, message: 'Failed to generate concept explanation', error: error.message });
+    }
 }
 
 module.exports = { generateInterviewQuestions , generateConceptExplanation };
